6a80d55cd26e1ede993990bf9dcddd8698ba4d36
Yumi <yogurt0313@gmail.com>
2014-05-27 20:21:12 +0800

    second

--- db/migrate/20140526133544_create_ideas.rb
+++ db/migrate/20140526133544_create_ideas.rb
@@ -2,7 +2,7 @@ class CreateIdeas < ActiveRecord::Migration
   def change
     create_table :ideas do |t|
       t.string :name
-      t.text :id
+      t.text :description
       t.string :picture
 
       t.timestamps
