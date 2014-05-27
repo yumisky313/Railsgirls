706665a69a38c5e106132dd017f4cb298a7a7039
Yumi <yogurt0313@gmail.com>
2014-05-27 22:15:04 +0800

    finished posts #index, #show

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,48 @@
+class PostsController < ApplicationController
+	def index
+		@posts = Post.all
+	end
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
+	def new
+		@post = Post.new
+	end
+
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def create
+	  @post = Post.new(posts_params)
+	  if @post.save
+	  	redirect_to @post
+	  else
+	  	render :action => :new
+	  end
+	end
+
+	def update
+		@post = Post.find(params[:id])
+		if @post.update(posts_params)
+			redirect_to @post
+		else
+			render :action => :edit
+		end
+	end
+
+	def destroy
+		@post = Post.find(params[:id])		
+		@post.destroy
+		redirect_to posts_url
+	end
+
+private
+
+def posts_params
+	params.require(:post).permit(:content, :author, :picture)	
+end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140526133544) do
+ActiveRecord::Schema.define(version: 20140527123533) do
 
   create_table "ideas", force: true do |t|
     t.string   "name"
     t.text     "description"
+    t.string   "picture"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "posts", force: true do |t|
+    t.text     "content"
+    t.string   "author"
     t.string   "picture"
     t.datetime "created_at"
     t.datetime "updated_at"
