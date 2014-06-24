2086dbf1b4a3ec3fa365507ef500f60df05f475a
Yumi <yogurt0313@gmail.com>
2014-06-24 21:24:26 +0800

    add something

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,12 @@
 class PostsController < ApplicationController
+before_action :get_post, :only => [:show, :edit, :update, :destroy ]
+
 	def index
 		@posts = Post.all
 	end
 
 	def show
-		@post = Post.find(params[:id])
+		#@post = Post.find(params[:id])
 	end
 
 	def new
@@ -12,7 +14,7 @@ class PostsController < ApplicationController
 	end
 
 	def edit
-		@post = Post.find(params[:id])
+		#@post = Post.find(params[:id])
 	end
 
 	def create
@@ -25,7 +27,7 @@ class PostsController < ApplicationController
 	end
 
 	def update
-		@post = Post.find(params[:id])
+		#@post = Post.find(params[:id])
 		if @post.update(posts_params)
 			redirect_to @post
 		else
@@ -34,12 +36,16 @@ class PostsController < ApplicationController
 	end
 
 	def destroy
-		@post = Post.find(params[:id])		
+		#@post = Post.find(params[:id])		
 		@post.destroy
 		redirect_to posts_url
 	end
 
 private
+
+def get_post
+			@post = Post.find(params[:id])		
+end
 
 def posts_params
 	params.require(:post).permit(:content, :author, :picture)	
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140603140500) do
+ActiveRecord::Schema.define(version: 20140624120443) do
+
+  create_table "boards", force: true do |t|
+    t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "ideas", force: true do |t|
     t.string   "name"
@@ -27,6 +33,7 @@ ActiveRecord::Schema.define(version: 20140603140500) do
     t.string   "picture"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "board_id"
   end
 
   create_table "users", force: true do |t|
